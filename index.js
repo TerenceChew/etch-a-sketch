a769bd965ccc050ac65810dd3f2be8ad5a4aadd6
TerenceChew <wschew97@gmail.com>
2022-08-05 02:00:19 -0400

    Update func sketch. Add event listener to window and squaresContainer

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function createSquares(gridSize) {
     // 25rem is the squaresContainer's width & height
     square.style.width = `calc(25rem / ${gridSize})`;
     square.style.height = `calc(25rem / ${gridSize})`;
-    square.addEventListener('mouseover', sketch, {once: true});
+    square.addEventListener('mousedown', sketch, {once: true});
     square.classList.add('square', 'center');    
     squaresContainer.append(square);
   }
@@ -20,12 +20,36 @@ function createSquares(gridSize) {
 
 // Change bg color of square
 function sketch(e) {
+  e.preventDefault();
   // console.log('sketch');
-  // console.log(e.type);
-  // e.preventDefault();
+  // console.log('ran', 'event is: ', e.type, e.buttons);
   let square = e.target;
   // square.textContent = '';
-  square.style.backgroundColor = 'pink';
+  if (e.buttons === 1) {
+    square.style.backgroundColor = 'pink';
+  }
 }
 
+// 'mousedown' on squaresContainer, add event listener to each square
+squaresContainer.addEventListener('mousedown', addEventListenerToSquares, true);
 
+function addEventListenerToSquares(e) {
+  // console.log('sc');
+  // console.log(e.type);
+  // console.log('add ran');
+  e.preventDefault();
+  const squares = Array.from(squaresContainer.children);
+  squares.forEach(square => square.addEventListener('mouseover', sketch, {once: true}))
+}
+
+// 'mouseup' on window, remove event listener from each square
+window.addEventListener('mouseup', removeEventListenerFromSquares);
+
+function removeEventListenerFromSquares(e) {
+  // console.log('rm ran');
+  const squares = Array.from(squaresContainer.children);
+  squares.forEach(square => square.removeEventListener('mouseover', sketch, {once: true}))
+}
+
+// Disable context menu from getting triggered in squaresContainer
+squaresContainer.addEventListener('contextmenu', (e) => e.preventDefault());
