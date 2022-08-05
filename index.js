22557ccd58b6db49713e29f5d2c9d75fd8bcebb7
TerenceChew <wschew97@gmail.com>
2022-08-04 22:38:38 -0400

    Update css & func createSquares.Add func sketch

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const squaresContainer = document.querySelector('.squares-container');
+// console.log(squaresContainer.width) ?? or style.width ??
+let gridSize = 75;
 
-let gridSize = 20;
+createSquares(gridSize);
 
+// Create and append squares to squaresContainer
 function createSquares(gridSize) {
-  for (let i = 0; i < gridSize * gridSize; i++) {
+  for (let i = 1; i <= gridSize * gridSize; i++) {
     const square = document.createElement('div');
-    square.textContent = 'S';
+    // square.textContent = `${i}`;
+    // 25rem is the squaresContainer's width & height
     square.style.width = `calc(25rem / ${gridSize})`;
     square.style.height = `calc(25rem / ${gridSize})`;
-    square.classList.add('square', 'center');
+    square.addEventListener('mouseover', sketch, {once: true});
+    square.classList.add('square', 'center');    
     squaresContainer.append(square);
   }
 }
 
-createSquares(gridSize);
+// Change bg color of square
+function sketch(e) {
+  // console.log('sketch');
+  // console.log(e.type);
+  // e.preventDefault();
+  let square = e.target;
+  // square.textContent = '';
+  square.style.backgroundColor = 'pink';
+}
+
+
