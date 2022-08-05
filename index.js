e0ee5170c44f6ec9f37693e12167754169197d62
TerenceChew <wschew97@gmail.com>
2022-08-05 02:11:31 -0400

    Update comments

--- index.js
+++ index.js
@@ -21,32 +21,30 @@ function createSquares(gridSize) {
 // Change bg color of square
 function sketch(e) {
   e.preventDefault();
-  // console.log('sketch');
-  // console.log('ran', 'event is: ', e.type, e.buttons);
+  console.log('sketch, event is:', e.type, 'key:', e.buttons);
   let square = e.target;
   // square.textContent = '';
+  // Only responds to left mousedown || mouseover
   if (e.buttons === 1) {
     square.style.backgroundColor = 'pink';
   }
 }
 
-// 'mousedown' on squaresContainer, add event listener to each square
+// mousedown on squaresContainer, add event listener to each square
 squaresContainer.addEventListener('mousedown', addEventListenerToSquares, true);
 
 function addEventListenerToSquares(e) {
-  // console.log('sc');
-  // console.log(e.type);
-  // console.log('add ran');
+  console.log('sC mousedown addEL, event is:', e.type, 'key:', e.buttons);
   e.preventDefault();
   const squares = Array.from(squaresContainer.children);
   squares.forEach(square => square.addEventListener('mouseover', sketch, {once: true}))
 }
 
-// 'mouseup' on window, remove event listener from each square
+// mouseup on window, remove event listener from each square
 window.addEventListener('mouseup', removeEventListenerFromSquares);
 
 function removeEventListenerFromSquares(e) {
-  // console.log('rm ran');
+  console.log('window mouseup removeEL, event is:', e.type, 'key:', e.buttons);
   const squares = Array.from(squaresContainer.children);
   squares.forEach(square => square.removeEventListener('mouseover', sketch, {once: true}))
 }
